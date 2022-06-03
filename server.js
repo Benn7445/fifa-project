c5b0ff5f609b94a014368f9a448bdc4180539d83
Kenny Vanbocxlaer <46047872+KennyVanBocxlaer@users.noreply.github.com>
2022-06-03 10:41:34 +0200

    test

--- server.js
+++ server.js
@@ -2,46 +2,57 @@ require("./backend/configs/db"); // De Mysql connection die wordt aangemaakt bij
 require("ejs");
 const app = require("express")();
 
-/**
- * Cors is de middleware dat wordt gebruikt bij ons project. Hierbij kunnen we zelf bepalen welke origins onze request kunnen gebruiken.
- * Dus niet alleen wanneer ze dezelfde ports hebben.
+/* 
+  Cors is de middleware dat wordt gebruikt bij ons project. Hierbij kunnen we zelf bepalen welke origins onze request kunnen gebruiken.
+  Dus niet alleen wanneer ze dezelfde ports hebben.
  */
 const cors = require("cors");
 app.use(cors());
 const express = require("express");
 const ejs = require("ejs"); // EJS import
+app.use("/public", express.static("public"));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs"); // EJS als view engine
-app.set("port", 5001);
+app.set("port", 3001);
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/Views/quiz.ejs", (req, res) => {
+app.get("/quiz", (req, res) => {
   res.render("quiz");
 });
-
-/**
+app.get("/leaderboard", (req, res) => {
+  res.render("leaderboard");
+});
+app.get("/favorite_leagues", (req, res) => {
+  res.render("favorite_leagues");
+});
+app.get("/favorite_clubs", (req, res) => {
+  res.render("favorite_clubs");
+});
+app.get("/blacklisted_clubs", (req, res) => {
+  res.render("blacklisted_clubs");
+});
+/*
  * Dit zorgt ervoor dat json kan worden uitgelezen in de body van de requests.
  */
 const bodyParser = require("express").json;
 app.use(bodyParser());
 
-/**
+/*
  * Dit zorgt ervoor dat de post request van de user js worden aangemaakt.
  */
 const UserRouter = require("./backend/api/user");
 app.use("/user", UserRouter);
 
-/**
- * Laat de app runnen op de port 5000.
+/* 
+Laat de app runnen op de port 3000.
  */
 app.listen(app.get("port"), () =>
-  console.log("[server] http://localhost:" + app.get("port"))
+  console.log("[server] http://localhost/:" + app.get("port"))
 );
-app.set('port', (process.env.PORT || 5000));
-const poort =  process.env.PORT;
-app.listen(poort, () => {
-  console.log("Fifa-Project-Backend server running op poost ${poort}.");
+const port = 
+app.listen(3000, () => {
+  console.log("Fifa-Project-Backend server running..");
 });
